2a72514ffd6f4336850b31ab34f43ea2f8a82389
bryanromeromtz <cryptomtz@hotmail.com>
2021-08-04 05:01:40 -0500

    settings for deployment to heroku

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// // Setting
+// app.set('port', 4000);
+
+// // Middlewares
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// console.log(__dirname);
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('bienvenidos');
+// })
+
+// app.listen(app.get('port'), () => {
+//   console.log(`Aplicacion escuchada en el puerto ${app.get('port')}`);
+
+// })
+
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = express.Router();
 const app = express();
-const path = require('path');
 
-// Setting
-app.set('port', 3000);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Middlewares
-app.use(express.static(path.join(__dirname, 'public')))
+router.get('/', (req, res) => {
+  // send homepage
+  res.sendFile(__dirname + '/public/');
+  console.log(__dirname);
 
-console.log(__dirname);
+});
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('bienvenidos');
-})
 
-app.listen(app.get('port'), () => {
-  console.log(`Aplicacion escuchada en el puerto ${app.get('port')}`);
+app.use('/', router);
 
-})
+app.listen(process.env.PORT || 3000);
+console.log(`App is listening at ${process.env.PORT || 3000}`);
