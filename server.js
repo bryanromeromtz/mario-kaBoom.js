9017da771650a5d03c5aa7329a6d842fc2f1c24e
bryanromeromtz <cryptomtz@hotmail.com>
2021-08-04 07:41:35 -0500

    fixing bugs

--- server.js
+++ server.js
@@ -1,45 +1,49 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// // Setting
+// app.set('port', 4000);
+
+// // Middlewares
+// app.use(express.static(path.join(__dirname, 'views')))
+
+// console.log(__dirname);
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('bienvenidos');
+// })
+
+// app.listen(app.get('port'), () => {
+//   console.log(`Aplicacion escuchada en el puerto ${app.get('port')}`);
+
+// })
+
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = express.Router();
 const app = express();
 const path = require('path');
 
-// Setting
-app.set('port', 4000);
 
-// Middlewares
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-console.log(__dirname);
+router.get('/', (req, res) => {
+  // send homepage
+  res.sendFile(__dirname + '/views/');
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('bienvenidos');
-})
+});
 
-app.listen(app.get('port'), () => {
-  console.log(`Aplicacion escuchada en el puerto ${app.get('port')}`);
-
-})
+router.use(express.static(path.join(__dirname, 'views')))
 
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const router = express.Router();
-// const app = express();
 
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router);
 
-// router.get('/', (req, res) => {
-//   // send homepage
-//   res.sendFile(__dirname + 'public');
-//   console.log(__dirname);
-
-// });
-
-
-// app.use('/', router);
-
-// app.listen(process.env.PORT || 3000);
-// console.log(`App is listening at ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 4000);
+console.log(`App is listening at ${process.env.PORT || 4000}`);
